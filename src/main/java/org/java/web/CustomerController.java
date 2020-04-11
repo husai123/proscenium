535d045b8147b0c11c1ee2b47b075eaa1af10380
1962197683 <1962197683@qq.com>
2020-04-11 20:28:13 +0800

    陈文俊提交

--- src/main/java/org/java/web/CustomerController.java
+++ src/main/java/org/java/web/CustomerController.java
@@ -1,8 +1,13 @@
 package org.java.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.Map;
 
 @Controller
 public class CustomerController {
@@ -17,6 +22,23 @@ public class CustomerController {
     public String load(){
         return "/index";
     }
+
+
+    @RequestMapping("/offer/{insurance_offer}")
+    public String offer(@RequestParam Map map,
+                        Model model,
+                        @PathVariable("insurance_offer")String insurance_offer){
+//        System.out.println(car_transfer);
+
+        if (map.get("car_transfer")==null){
+            map.put("car_transfer",1);
+        }
+
+        System.out.println(map);
+        model.addAttribute("userInsurance",map);
+        return insurance_offer;
+    }
+
 
     @RequestMapping("/personal")
     public String personal(){
